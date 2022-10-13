c3d8ff377a9ff09a6c5348d01e274edec4cdaaf3
fygure <92011471+fygure@users.noreply.github.com>
2022-10-12 23:05:56 -0500

    registered slash command

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -1,1 +1,23 @@
 import { REST } from "@discordjs/rest"
+import { SlashCommandBuilder } from "@discordjs/builders"
+import { Routes } from "discord-api-types/v9"
+import config from "./config"
+
+// List of commands
+const commands = [
+    new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("pong!")
+];
+
+// Register command
+const rest = new REST( { version: '9' }).setToken(config.DISCORD_TOKEN)
+
+// Call put function to register command
+rest.put(Routes.applicationGuildCommands(config.CLIENT_ID, config.GUILD_ID), {body: commands})
+.then( () => {
+    console.log("Successfully registered application commands")
+}).catch(console.error)
+
+// Now call applicationGuildCommands register thing
+// Create new script in package.json
