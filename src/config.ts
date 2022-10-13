2f43d4483b1d75a8c9e629b85e325b1353ab2b60
fygure <92011471+fygure@users.noreply.github.com>
2022-10-12 22:50:30 -0500

    created config.ts which loads in env variables

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@
 
 import dotenv from "dotenv"
 dotenv.config()
-const { CLIENT_ID, GUILD_ID, DISCORD_TOKEN } = process.env
+const { CLIENT_ID, GUILD_ID, DISCORD_TOKEN } = process.env;
 
 // Verify loading of env variables
 if (!CLIENT_ID || !GUILD_ID || !DISCORD_TOKEN) {
