99e7a3a12966bc6c4ff7b052fd6904a06769ef69
fygure <92011471+fygure@users.noreply.github.com>
2022-10-12 22:59:58 -0500

    added bot.ts for basic commands, defining the commands in deploy-commands.ts

---
+++ src/bot.ts
@@ -0,0 +1,30 @@
+// Code for the bot
+
+import { Client } from "discord.js"
+import config from "./config"
+
+// Define new instance of the client to use in REST API
+// intents - permissions that the bot requires
+export const client = new Client({ 
+    intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"] 
+});
+
+
+// Event listener
+client.once("ready", () => {
+    console.log("Discord bot ready!");
+});
+
+// Event listener
+// Register the ping command -> create a new file deploy-commands.ts
+client.on('interactionCreate', async interaction => {
+    if(!interaction.isCommand()){
+        return
+    }
+    const { commandName } = interaction;
+    if (commandName == 'ping') {
+        return interaction.reply("pong")
+    }
+})
+
+client.login(config.DISCORD_TOKEN)
