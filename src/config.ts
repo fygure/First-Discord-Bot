dcb1ed0424a0fb069a1c4c9d64f02739f02dc5b3
fygure <92011471+fygure@users.noreply.github.com>
2022-10-12 22:50:02 -0500

    created config.ts which loads in env variables

---
+++ src/config.ts
@@ -0,0 +1,20 @@
+// Centralized place that verifies no missing env variables
+// Provides convenient way to load them from other modules
+
+import dotenv from "dotenv"
+dotenv.config()
+const { CLIENT_ID, GUILD_ID, DISCORD_TOKEN } = process.env
+
+// Verify loading of env variables
+if (!CLIENT_ID || !GUILD_ID || !DISCORD_TOKEN) {
+    throw new Error("Missing environment variables")
+}
+
+// Store env variables
+const config : Record<string, string> = {
+    CLIENT_ID,
+    GUILD_ID,
+    DISCORD_TOKEN
+}
+
+export default config
